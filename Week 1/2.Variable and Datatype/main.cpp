a6bdcecb0edeaf5c3f8c1f9efa22b78eddaa79e0
Ankit Jha <ankitjhacse2003@gmail.com>
2025-09-12 18:39:37 +0530

    Initial commit - DSA Supreme Batch Practice

---
+++ Week 1/2.Variable and Datatype/main.cpp
@@ -0,0 +1,7 @@
+# include<iostream>
+using namespace std;
+int main (){
+    int age = 22;
+    cout<<"My age is "<<age<<endl;
+    return 0;
+}
---
+++ Week 1/3.User Input/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int marks;
+    cout<<"Enter Your Marks"<<endl;
+
+    cin>>marks;
+    cout <<"Ankit Jha Marks "<< marks<<endl; 
+    return 0;
+    
+}
